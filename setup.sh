3dea990ee65e42448e446680230a045fcbb97ebc
Aaron Nichols <anichols@sugarsync.com>
2011-04-17 10:36:45 -0700

    Really added setup.sh this time

---
+++ setup.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+# Install tmux configuration & scripts
+
+# First - just sanity check that we're in our setup directory
+if [ ! -f .2993b6b548000a80989a20549e7558a5 ] ; then
+  echo "Please run this script from the same directory the setup.sh is in"
+  echo "If you are doing that, make sure the package is intact because a"
+  echo "file called \".2993b6b548000a80989a20549e7558a5\" is missing"
+  exit
+fi
+
+# Install scripts
+read -p "By default scripts are stored in ~/bin - would you like them here? [y/n]: " CONT
+
+if [ $CONT == 'y' -o $CONT == 'Y' ] ; then
+  SCRIPT_DIR='~/bin'
+else
+  read -p "OK, what directory would you like them in? : " NEW_DIR
+  echo "Using \"$NEW_DIR\" for scripts"
+  SCRIPT_DIR=$NEW_DIR
+fi
+
+# Check to see if specified directory exists
+if [ ! -d $SCRIPT_DIR ] ; then
+  read -p "Directory \"$SCRIPT_DIR\" does not appear to exist, should I create it? [y/n]: " CREATE_DIR
+
+# If we need to create the directory - we check with the user & then do it
+  if [ $CREATE_DIR == 'y' -o $CREATE_DIR == 'Y' ] ; then
+    echo "This will create the directory \"$SCRIPT_DIR\" relative to your CWD - currently \"$CWD\""
+    read -p "Press ENTER to continue: " CONT
+    mkdir $SCRIPT_DIR
+
+# We can't really proceed w/out a destination directory
+  else
+    echo "Aborting, cannot copy scripts if destination directory does not exist"
+    exit
+  fi
+
+else
+# Start copying scripts
+  for f in bin/*
+  do
+    cp -i $f $SCRIPT_DIR/$f
+  done
+
+# Discover what aliases file, if any, is in use
+
+# First check .bashrc
+  ALIASES=egrep -m 1 -o ' \S*\.\S*alias\S*' ~/.bashrc
+
+# If that guy doesn't return anything, check bash_profile
+  if [ -z $ALIASES ] ; then
+    ALIASES=egrep -m 1 -o ' \S*\.\S*alias\S*' ~/.bash_profile
+  fi
+
+# If the variable is still empty we abort, otherwise append to aliases file
+  if [ -z $ALIASES ] ; then
+    echo "Sorry, couldn't determine where to put your alias, please follow the manual instructions for that part"
+    exit
+  else 
+    cat aliases >> $ALIASES
+  fi
+fi
